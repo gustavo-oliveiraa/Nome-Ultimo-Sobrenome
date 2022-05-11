851b4b253507684e2a0e4df15da6755e0ecd5a86
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2022-05-11 10:01:26 -0300

    Update Link para Acessar os sites..cpp

--- Link para Acessar os sites..cpp
+++ Link para Acessar os sites..cpp
@@ -1,1 +1,4 @@
 //Link para Acessar os sites.
+
+https://www.clubedohardware.com.br/forums/topic/1257345-algoritmo-que-informe-ultimo-sobrenome/
+https://www.clubedohardware.com.br/forums/topic/1503366-imprimir-primeiro-e-ultimo-nome/
